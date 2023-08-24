5e4c95780e11b4a03ae482d3edb72fcaa3c41e8b
Ramon <ramonmatheus1988@gmail.com>
2023-08-24 13:15:12 -0300

    subida del proyecto

---
+++ src/main/java/com/api/challenge/repositories/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.api.challenge.repositories;
+
+import com.api.challenge.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, Long> {
+}
